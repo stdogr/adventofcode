25ebbc5eb8ec80f22439b9084c7e0d5e7d08fb7c
Stefan Greis <stefan.d.greis@gmail.com>
2021-12-04 21:13:21 +0100

    reduce duplication

--- src/main/kotlin/stdogr/aoc/Day3FirstPart.kt
+++ src/main/kotlin/stdogr/aoc/Day3FirstPart.kt
@@ -3,43 +3,33 @@ package stdogr.aoc
 import kotlin.math.pow
 
 fun findEpsilon(bits: List<String>): Int {
-    var result = ""
-    for (index in 0 until 5) {
-        val (zeroes, ones) = countBits(bits, index)
-        if (zeroes < ones) {
-            result += 0
-        } else {
-            result += 1
-        }
+    return produceSequenceByMostCommonBit(bits) { zeroes, ones ->
+        if (zeroes < ones) 0 else 1
     }
-    return convertToInt(result)
 }
 
 fun findGamma(bits: List<String>): Int {
-    var result = ""
-    for (index in 0 until 5) {
-        val (zeroes, ones) = countBits(bits, index)
-        if (zeroes > ones) {
-            result += 0
-        } else {
-            result += 1
-        }
+    return produceSequenceByMostCommonBit(bits) { zeroes, ones ->
+        if (zeroes > ones) 0 else 1
     }
-    return convertToInt(result)
 }
 
-fun countBits(bits: List<String>, index: Int): Pair<Int, Int> {
-    var zeroes = 0
-    var ones = 0
-    bits.forEach {
-        val bit = it[index]
-        if (bit == '0') {
-            zeroes += 1
-        } else if (bit == '1') {
-            ones += 1
+private fun produceSequenceByMostCommonBit(bits: List<String>, pickZeroOrOne: (Int, Int) -> Int): Int {
+    var result = ""
+    for (index in 0 until 5) {
+        var zeroes = 0
+        var ones = 0
+        bits.forEach {
+            val bit = it[index]
+            if (bit == '0') {
+                zeroes += 1
+            } else if (bit == '1') {
+                ones += 1
+            }
         }
+        result += pickZeroOrOne(zeroes, ones)
     }
-    return zeroes to ones
+    return convertToInt(result)
 }
 
 private fun convertToInt(bits: String): Int {
