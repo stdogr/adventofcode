aeaf2d4d52ee6d8f503b9f7279f3212f99ba6d2a
Stefan Greis <stefan.d.greis@gmail.com>
2021-12-04 18:57:01 +0100

    added day 1 part 2

--- src/main/kotlin/stdogr/aoc/Day1.kt
+++ src/main/kotlin/stdogr/aoc/Day1.kt
@@ -12,12 +12,12 @@ class Loader {
         val values = data.lines()
             .filter { it.isNotBlank() }
             .map { it.toInt() }
-        val count = countNumberOfTimeValuesIncrease(values)
+        val count = countNumberOfTimesValuesIncrease(values)
         println(count)
     }
 }
 
-fun countNumberOfTimeValuesIncrease(values: List<Int>): Int {
+fun countNumberOfTimesValuesIncrease(values: List<Int>): Int {
     if (values.isEmpty()) return 0
     var count = 0
     for (index in 1 until values.size) {
@@ -26,4 +26,16 @@ fun countNumberOfTimeValuesIncrease(values: List<Int>): Int {
         if (previous < current) count += 1
     }
     return count
+}
+
+fun countNumberOfTimesValuesIncreaseWindowed(values: List<Int>): Int {
+    if (values.size < 4) return 0
+    val windows = mutableListOf<Int>()
+    for (index in 2 until values.size) {
+        val first = values[index - 2]
+        val second = values[index - 1]
+        val current = values[index]
+        windows.add(first + second + current)
+    }
+    return countNumberOfTimesValuesIncrease(windows)
 }
--- src/test/kotlin/stdogr/aoc/Day1Test.kt
+++ src/test/kotlin/stdogr/aoc/Day1Test.kt
@@ -6,16 +6,30 @@ import org.junit.jupiter.api.Test
 class Day1Test {
 
     @Test
-    fun `check input includes 7 increased values`() {
+    fun `count number of times values increase`() {
         // given
         val values = listOf(
             199, 200, 208, 210, 200, 207, 240, 269, 260, 263
         )
 
         // when
-        val count = countNumberOfTimeValuesIncrease(values)
+        val count = countNumberOfTimesValuesIncrease(values)
 
         // then
         Assertions.assertThat(count).isEqualTo(7)
+    }
+
+    @Test
+    fun `count number of times values increase windowed`() {
+        // given
+        val values = listOf(
+            199, 200, 208, 210, 200, 207, 240, 269, 260, 263,
+        )
+
+        // when
+        val count = countNumberOfTimesValuesIncreaseWindowed(values)
+
+        // then
+        Assertions.assertThat(count).isEqualTo(5)
     }
 }
