a6f73919423c2c3b1113203e15de8006d6bb7942
Stefan Greis <stefan.d.greis@gmail.com>
2021-12-04 20:29:45 +0100

    day 2 part 2

--- src/main/kotlin/stdogr/aoc/Day2SecondPart.kt
+++ src/main/kotlin/stdogr/aoc/Day2SecondPart.kt
@@ -5,20 +5,28 @@ import stdogr.aoc.util.FileLoader
 fun main() {
     val data = FileLoader().load("2021/day2.txt")
     val commands = data.lines().filter { it.isNotBlank() }
-    val result = processCommands(commands)
+    val result = processCommandsWithAim(commands)
     println(result)
 }
 
-fun processCommands(commands: List<String>): Int {
+fun processCommandsWithAim(commands: List<String>): Int {
     var horizontal = 0
     var depth = 0
+    var aim = 0
     commands.forEach {
         val (command, numberString) = it.split(" ")
         val number = numberString.toInt()
         when (command) {
-            "forward" -> horizontal += number
-            "up" -> depth -= number
-            "down" -> depth += number
+            "forward" -> {
+                horizontal += number
+                depth += aim * number
+            }
+            "up" -> {
+                aim -= number
+            }
+            "down" -> {
+                aim += number
+            }
             else -> throw IllegalArgumentException("Received illegal command: [$command]!")
         }
     }
---
+++ src/test/kotlin/stdogr/aoc/Day2SecondPartKtTest.kt
@@ -0,0 +1,27 @@
+package stdogr.aoc
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day2SecondPartKtTest {
+
+    @Test
+    fun `process commands`() {
+        // given
+        val commands = listOf(
+            "forward 5",
+            "down 5",
+            "forward 8",
+            "up 3",
+            "down 8",
+            "forward 2",
+        )
+
+        // when
+        val result = processCommandsWithAim(commands)
+
+        // then
+        Assertions.assertThat(result).isEqualTo(900)
+    }
+}
+
