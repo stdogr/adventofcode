b1104cd6fc8eadbb2df8a04146c7f016e67edb29
Stefan Greis <stefan.d.greis@gmail.com>
2021-12-22 14:46:08 +0100

    day 9: rework neighbors function, added structure for point

--- src/main/kotlin/stdogr/aoc2021/Day9.kt
+++ src/main/kotlin/stdogr/aoc2021/Day9.kt
@@ -29,22 +29,59 @@ fun sumRiskLevels(data: String): Int {
     return lowPoints.sumOf { it + 1 }
 }
 
+fun sumThreeLargestBasins(data: String): Int {
+    val heightMap: List<List<Int>> = data.lines()
+        .filterNot { it.isBlank() }
+        .map { line -> line.toCharArray().map { value -> "$value".toInt() } }
+
+    val lowPoints = mutableListOf<Int>()
+
+    heightMap.forEachIndexed { y, row ->
+        row.forEachIndexed { x, value ->
+            if (isLowPoint(value, x, row.size, y, heightMap)) {
+                initBasin(y, x, row.size - 1, heightMap)
+            }
+        }
+    }
+
+    return lowPoints.sumOf { it + 1 }
+}
+
 private fun isLowPoint(value: Int, x: Int, rowSize: Int, y: Int, heightMap: List<List<Int>>): Boolean {
-    return neighbors(x, rowSize - 1, y, heightMap.size - 1, heightMap)
+    return neighborValues(x, rowSize - 1, y, heightMap.size - 1, heightMap)
         .find { it <= value } == null
 }
 
-private fun neighbors(x: Int, maxX: Int, y: Int, maxY: Int, heightMap: List<List<Int>>): List<Int> {
-    val neighbors = mutableListOf<Int>()
+fun initBasin(y: Int, x: Int, maxX: Int, heightMap: List<List<Int>>) {
+    val basin = mutableListOf<Point>()
+    basin.add(Point(x, y))
+
+    for (point in basin) {
+        neighbors(point.x, maxX, point.y, heightMap.size - 1)
+    }
+}
+
+private fun neighbors(x: Int, maxX: Int, y: Int, maxY: Int): List<Point> {
+    val neighbors = mutableListOf<Point>()
     val left = x - 1
     val right = x + 1
     val up = y - 1
     val down = y + 1
 
-    if (left >= 0) neighbors.add(heightMap[y][left])
-    if (right <= maxX) neighbors.add(heightMap[y][right])
-    if (up >= 0) neighbors.add(heightMap[up][x])
-    if (down <= maxY) neighbors.add(heightMap[down][x])
+    if (left >= 0) neighbors.add(Point(left, y))
+    if (right <= maxX) neighbors.add(Point(right, y))
+    if (up >= 0) neighbors.add(Point(x, up))
+    if (down <= maxY) neighbors.add(Point(x, down))
 
     return neighbors
 }
+
+private fun neighborValues(x: Int, maxX: Int, y: Int, maxY: Int, heightMap: List<List<Int>>): List<Int> {
+    return neighbors(x, maxX, y, maxY)
+        .map { heightMap[it.y][it.x] }
+}
+
+data class Point(
+    val x: Int,
+    val y: Int
+)
