2ddb30c45ec13e343827b6cc7d177952fd486508
Stefan Greis <stefan.d.greis@gmail.com>
2022-12-10 19:40:45 +0100

    cleanup

--- src/main/kotlin/stdogr/aoc2022/Day1.kt
+++ src/main/kotlin/stdogr/aoc2022/Day1.kt
@@ -4,29 +4,23 @@ package stdogr.aoc2022
 class Day1 : Day<Int>("day1") {
 
     override fun partOne(input: String): Int {
-        val lines = input
-            .lines()
+        val numbers = sum(input)
 
-        val numbers = mutableListOf<Int>()
-
-        var current = 0
-        lines.forEach {
-            if (it.isNotBlank()) {
-                current += it.toInt()
-            } else {
-                numbers.add(current)
-                current = 0
-            }
-        }
         return numbers.maxOf { it }
     }
 
     override fun partTwo(input: String): Int {
+        val numbers = sum(input)
+        val sorted = numbers.sortedDescending()
+
+        return sorted.take(3).sum()
+    }
+
+    private fun sum(input: String): MutableList<Int> {
         val lines = input
             .lines()
 
         val numbers = mutableListOf<Int>()
-
         var current = 0
         lines.forEach {
             if (it.isNotBlank()) {
@@ -36,9 +30,6 @@ class Day1 : Day<Int>("day1") {
                 current = 0
             }
         }
-
-        val sorted = numbers.sortedDescending()
-
-        return sorted[0] + sorted[1] + sorted[2]
+        return numbers
     }
 }
--- src/main/kotlin/stdogr/aoc2022/Day2.kt
+++ src/main/kotlin/stdogr/aoc2022/Day2.kt
@@ -1,39 +1,32 @@
 package stdogr.aoc2022
 
 class Day2 : Day<Int>("day2") {
+
+    private val outcomeScore = mapOf(
+        "A" to mapOf("X" to 3, "Y" to 6, "Z" to 0),
+        "B" to mapOf("X" to 0, "Y" to 3, "Z" to 6),
+        "C" to mapOf("X" to 6, "Y" to 0, "Z" to 3),
+    )
+
+    private val shapeScore = mapOf("X" to 1, "Y" to 2, "Z" to 3)
+
     override fun partOne(input: String): Int {
-        val matrix = mapOf(
-            "A" to mapOf("X" to 3, "Y" to 6, "Z" to 0),
-            "B" to mapOf("X" to 0, "Y" to 3, "Z" to 6),
-            "C" to mapOf("X" to 6, "Y" to 0, "Z" to 3),
-        )
-
-        val score = mapOf("X" to 1, "Y" to 2, "Z" to 3)
-
         return input
             .lines()
             .filter { it.isNotBlank() }
             .sumOf {
                 val shapes = it.split(" ")
 
-                matrix[shapes[0]]!![shapes[1]]!! + score[shapes[1]]!!
+                outcomeScore[shapes[0]]!![shapes[1]]!! + shapeScore[shapes[1]]!!
             }
     }
 
     override fun partTwo(input: String): Int {
-        val matrix = mapOf(
-            "A" to mapOf("X" to 3, "Y" to 6, "Z" to 0),
-            "B" to mapOf("X" to 0, "Y" to 3, "Z" to 6),
-            "C" to mapOf("X" to 6, "Y" to 0, "Z" to 3),
-        )
-
         val translate = mapOf(
             "X" to mapOf("A" to "Z", "B" to "X", "C" to "Y"),
             "Y" to mapOf("A" to "X", "B" to "Y", "C" to "Z"),
             "Z" to mapOf("A" to "Y", "B" to "Z", "C" to "X"),
         )
-
-        val score = mapOf("X" to 1, "Y" to 2, "Z" to 3)
 
         return input
             .lines()
@@ -44,7 +37,7 @@ class Day2 : Day<Int>("day2") {
                 val command = shapes[1]
                 val shape = translate[command]!![opponent]
 
-                matrix[opponent]!![shape]!! + score[shape]!!
+                outcomeScore[opponent]!![shape]!! + shapeScore[shape]!!
             }
     }
 }
