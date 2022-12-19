27f6f598680a4fa7fd87edc2a3219d074b22be0b
Stefan Greis <stefan.d.greis@gmail.com>
2022-12-19 15:51:54 +0100

    add test

---
+++ src/test/kotlin/stdogr/aoc2022/AdventOfCodeTest.kt
@@ -0,0 +1,32 @@
+package stdogr.aoc2022
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import stdogr.util.loadResource
+import java.util.stream.Stream
+
+class AdventOfCodeTest {
+
+    @ParameterizedTest
+    @MethodSource("days")
+    fun <T> `validate day`(day: Task<T>, expectedResultPartOne: T, expectedResultPartTwo: T) {
+        val input = loadResource("2022/${day.filePrefix}_example.txt")
+        assertThat(day.partOne(input)).isEqualTo(expectedResultPartOne)
+        assertThat(day.partTwo(input)).isEqualTo(expectedResultPartTwo)
+    }
+
+    companion object {
+        @JvmStatic
+        fun days(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(Day1(), 24000, 41000),
+                Arguments.of(Day2(), 15, 12),
+                Arguments.of(Day3(), 157, 70),
+                Arguments.of(Day4(), 2, 4),
+                Arguments.of(Day5(), "CMZ", "MCD"),
+            )
+        }
+    }
+}
