0312639b0ce0a54ab06e0e940164aa74ae204038
Stefan Greis <stefan.d.greis@gmail.com>
2022-12-20 16:50:20 +0100

    day 9 part 2

--- src/main/kotlin/stdogr/aoc2022/Day9.kt
+++ src/main/kotlin/stdogr/aoc2022/Day9.kt
@@ -5,13 +5,21 @@ import kotlin.math.abs
 class Day9 : Task<Int>("9_rope_bridge") {
 
     override fun partOne(input: String): Int {
-        val moveRegex = "([RLUD]) ([0-9]+)".toRegex()
+        return move(input, 2)
+    }
 
+    override fun partTwo(input: String): Int {
+        return move(input, 10)
+    }
+
+    private fun move(input: String, ropeLength: Int): Int {
+        val moveRegex = "([RLUD]) ([0-9]+)".toRegex()
+        val rope = mutableListOf<Pair<Int, Int>>()
+        repeat(ropeLength) {
+            rope.add(Pair(10000000, 10000000)) // lazy fix to avoid negative coordinates
+        }
         val visited = mutableSetOf<Pair<Int, Int>>()
-        // laziest fix for movement with negative coordinates
-        var head = Pair(10000000, 10000000)
-        var tail = Pair(10000000, 10000000)
-        visited.add(tail)
+        visited.add(rope.last())
 
         input.lines()
             .forEach { line ->
@@ -20,46 +28,78 @@ class Day9 : Task<Int>("9_rope_bridge") {
                 val steps = find.groups[2]!!.value.toInt()
 
                 repeat(steps) {
-                    when (direction) {
-                        "U" -> {
-                            head = Pair(head.first, head.second + 1)
-                            if (abs(abs(head.second) - abs(tail.second)) > 1) {
-                                tail = Pair(head.first, head.second - 1)
-                                visited.add(tail)
+                    rope.forEachIndexed { index, _ ->
+                        if (index == 0) {
+                            when (direction) {
+                                "U" -> rope[index] = Pair(rope[index].first, rope[index].second + 1)
+                                "D" -> rope[index] = Pair(rope[index].first, rope[index].second - 1)
+                                "L" -> rope[index] = Pair(rope[index].first - 1, rope[index].second)
+                                "R" -> rope[index] = Pair(rope[index].first + 1, rope[index].second)
                             }
-                        }
-
-                        "D" -> {
-                            head = Pair(head.first, head.second - 1)
-                            if (abs(abs(head.second) - abs(tail.second)) > 1) {
-                                tail = Pair(head.first, head.second + 1)
-                                visited.add(tail)
-                            }
-                        }
-
-                        "L" -> {
-                            head = Pair(head.first - 1, head.second)
-                            if (abs(abs(head.first) - abs(tail.first)) > 1) {
-                                tail = Pair(head.first + 1, head.second)
-                                visited.add(tail)
-                            }
-                        }
-
-                        "R" -> {
-                            head = Pair(head.first + 1, head.second)
-                            if (abs(abs(head.first) - abs(tail.first)) > 1) {
-                                tail = Pair(head.first - 1, head.second)
-                                visited.add(tail)
-                            }
+                        } else {
+                            move(rope, index)
                         }
                     }
+                    visited.add(rope.last())
+
                 }
             }
-
         return visited.size
     }
 
-    override fun partTwo(input: String): Int {
-        TODO()
+    private fun move(rope: MutableList<Pair<Int, Int>>, index: Int): String {
+        val previousSegment = rope[index - 1]
+        val currentSegment = rope[index]
+
+        if (abs(abs(previousSegment.first) - abs(currentSegment.first)) > 1) {
+            if (previousSegment.first > currentSegment.first) {
+                if (previousSegment.second > currentSegment.second) {
+                    rope[index] = Pair(currentSegment.first + 1, currentSegment.second + 1)
+                    return "RU"
+                } else if (previousSegment.second < currentSegment.second) {
+                    rope[index] = Pair(currentSegment.first + 1, currentSegment.second - 1)
+                    return "RD"
+                } else {
+                    rope[index] = Pair(currentSegment.first + 1, currentSegment.second)
+                    return "R"
+                }
+            } else {
+                if (previousSegment.second > currentSegment.second) {
+                    rope[index] = Pair(currentSegment.first - 1, currentSegment.second + 1)
+                    return "LU"
+                } else if (previousSegment.second < currentSegment.second) {
+                    rope[index] = Pair(currentSegment.first - 1, currentSegment.second - 1)
+                    return "LD"
+                } else {
+                    rope[index] = Pair(currentSegment.first - 1, currentSegment.second)
+                    return "L"
+                }
+            }
+        } else if (abs(abs(previousSegment.second) - abs(currentSegment.second)) > 1) {
+            if (previousSegment.second > currentSegment.second) {
+                if (previousSegment.first > currentSegment.first) {
+                    rope[index] = Pair(currentSegment.first + 1, currentSegment.second + 1)
+                    return "RU"
+                } else if (previousSegment.first < currentSegment.first) {
+                    rope[index] = Pair(currentSegment.first - 1, currentSegment.second + 1)
+                    return "LU"
+                } else {
+                    rope[index] = Pair(currentSegment.first, currentSegment.second + 1)
+                    return "U"
+                }
+            } else {
+                if (previousSegment.first > currentSegment.first) {
+                    rope[index] = Pair(currentSegment.first + 1, currentSegment.second - 1)
+                    return "RD"
+                } else if (previousSegment.first < currentSegment.first) {
+                    rope[index] = Pair(currentSegment.first - 1, currentSegment.second - 1)
+                    return "LD"
+                } else {
+                    rope[index] = Pair(currentSegment.first, currentSegment.second - 1)
+                    return "D"
+                }
+            }
+        }
+        return "not moved"
     }
 }
