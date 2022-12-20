e220ceb4c766c22c339dd38c5c4ca94d57fe2e53
Stefan Greis <stefan.d.greis@gmail.com>
2022-12-20 20:04:35 +0100

    day 10 part 2

--- src/main/kotlin/stdogr/aoc2022/Day10.kt
+++ src/main/kotlin/stdogr/aoc2022/Day10.kt
@@ -32,6 +32,51 @@ class Day10 : Task<Int>("10_cathode_ray_tube") {
     }
 
     override fun partTwo(input: String): Int {
-        TODO()
+        val regex = "addx (-?)([0-9]+)".toRegex()
+
+        val cycles = mutableListOf<Pair<Int, Int>>()
+        cycles.add(1 to 1)
+
+        val pixels = mutableListOf<String>()
+        val crtPixelIndex = CrtPixelIndex()
+
+        input.lines()
+            .forEach { line ->
+                if (line == "noop") {
+                    val lastCycle = cycles.last()
+                    draw(crtPixelIndex, pixels, lastCycle)
+                    cycles.add(lastCycle.first + 1 to lastCycle.second)
+                } else {
+                    val find = regex.find(line)!!
+                    val multiplier = if (find.groups[1]!!.value == "") 1 else -1
+                    val value = find.groups[2]!!.value.toInt() * multiplier
+
+                    val lastCycle = cycles.last()
+                    draw(crtPixelIndex, pixels, lastCycle)
+                    cycles.add(lastCycle.first + 1 to lastCycle.second)
+                    draw(crtPixelIndex, pixels, cycles.last())
+                    cycles.add(lastCycle.first + 2 to lastCycle.second + value)
+                }
+            }
+
+        pixels.chunked(40)
+            .forEach { println(it.joinToString("")) }
+
+        return 0
+    }
+
+    private fun draw(crtPixelIndex: CrtPixelIndex, pixels: MutableList<String>, lastCycle: Pair<Int, Int>) {
+        val spriteArea = listOf(lastCycle.second - 1, lastCycle.second, lastCycle.second + 1)
+        pixels.add(if (crtPixelIndex.value() in spriteArea) "#" else ".")
+    }
+
+    class CrtPixelIndex {
+        private var value: Int = 0
+
+        fun value(): Int {
+            val currentValue = value
+            if (value == 39) value = 0 else value += 1
+            return currentValue
+        }
     }
 }
